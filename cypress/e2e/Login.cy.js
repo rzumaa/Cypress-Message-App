f231382507e1b4f4f7a074b16d22cfbe9c711f69
Paula <paulinakocha@gmail.com>
2022-11-17 19:39:35 +0100

    Change login test

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -17,35 +17,35 @@ describe('Login into the application', () => {
         cy.get('button[title="Wyloguj"]').click();
 });
 
-    it('Login is wrong', () => {
+    it('Should error for an invalid user', () => {
         cy.fixture('LoginData').then(function (LoginData) {
             this.LoginData = LoginData;
-            cy.get('input[name="email"]').type(this.LoginData.wrongemail)//.should('be.value', 'tesst@gmail.com' );
+            cy.get('input[name="email"]').type(this.LoginData.wrongemail)//.should('be.value', this.LoginData.email );
             cy.get('input[name="password"]').type(this.LoginData.wrongpassword)//.should('be.value', '123323');
             cy.get('button[type=submit]').as('sbmitBtn').click();
 
         cy.wait(3000);
     });
 });
-    it('Login without password', () => {
+    it('Should error for empty password', () => {
         cy.fixture('LoginData').then(function (LoginData) {
             this.LoginData = LoginData;
-            cy.get('input[name="email"]').type(this.LoginData.email)
+            cy.get('input[name="email"]').type(this.LoginData.email);
         
             cy.get('button[type=submit]').as('sbmitBtn').click();
-            cy.contains('Hasło jest wymagane').should('be.visible')
+            cy.contains('Hasło jest wymagane').should('be.visible');
 
         cy.wait(3000);
     });
 });
 
-    it('Login without email', () => {
+    it('Should error for empty login', () => {
         cy.fixture('LoginData').then(function (LoginData) {
             this.LoginData = LoginData;
             cy.get('input[name="password"]').type(this.LoginData.password)
         
             cy.get('button[type=submit]').as('sbmitBtn').click();
-            cy.contains('E-mail jest wymagany').should('be.visible')
+            cy.contains('E-mail jest wymagany').should('be.visible');
 
         cy.wait(3000);
     });
@@ -55,9 +55,9 @@ describe('Login into the application', () => {
         cy.fixture('LoginData').then(function (LoginData) {
             this.LoginData = LoginData;
             cy.get('input[name="email"]').type(this.LoginData.email)
-            cy.get('input[name="password"]').type('12345')
+            cy.get('input[name="password"]').type('12345').should('have.length', 8);
     
-            cy.get('button[type=submit]').as('sbmitBtn').click();
+            cy.get('button[type=submit]').as('sbmitBtn').click()
             cy.contains('Hasło musi zawierać przynajmniej 8 znaków').should('be.visible')
 
         cy.wait(3000);
