c9d7116dd07ee562ca30796426ad6659e82169d1
Paula <paulinakocha@gmail.com>
2022-11-17 18:57:19 +0100

    Change login test

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,54 +1,76 @@
 describe('Login into the application', () => {
-    it('Login is correct', () => {
+    beforeEach(() => {
         cy.viewport(1920,1080);
         cy.visit('/login');
-        cy.contains('h1', 'Zaloguj się!')
-        .should('be.visible');
+        cy.contains('h1', 'Zaloguj się!').should('be.visible');
+    })
 
-    cy.fixture('LoginData').then(function (LoginData) {
-        this.LoginData = LoginData;
-        cy.get('input[name="email"]').type(this.LoginData.email);
-        cy.get('input[name="password"]').type(this.LoginData.password);
-        cy.get('button[type=submit]').as('sbmitBtn').click();
-    });
-    cy.contains('.MuiButton-label', 'Zaloguj się')
-        .click();
-    cy.wait(4000);
-    cy.get('button[title="Wyloguj"]').click();
-});
-
-    it('Login i s wrong', () => {
-        cy.viewport(1920,1080);
-        cy.visit('/login');
-        cy.contains('h1', 'Zaloguj się!')
-            .should('be.visible');
-
+   it('Login is correct', () => {
         cy.fixture('LoginData').then(function (LoginData) {
             this.LoginData = LoginData;
-            cy.get('input[name="email"]')
-                .type(this.LoginData.wrongemail);
-            //.should('be.value', 'tesst@gmail.com' );
+            cy.get('input[name="email"]').type(this.LoginData.email);
+            cy.get('input[name="password"]').type(this.LoginData.password);
+            cy.get('button[type=submit]').as('sbmitBtn').click();
+    });
+        cy.contains('.MuiButton-label', 'Zaloguj się').click();
+        cy.wait(5000);
+        cy.get('button[title="Wyloguj"]').click();
+});
 
-        cy.get('input[name="password"]')
-            .type(this.LoginData.wrongpassword);
+    it('Login is wrong', () => {
+        cy.fixture('LoginData').then(function (LoginData) {
+            this.LoginData = LoginData;
+            cy.get('input[name="email"]').type(this.LoginData.wrongemail)//.should('be.value', 'tesst@gmail.com' );
+            cy.get('input[name="password"]').type(this.LoginData.wrongpassword)//.should('be.value', '123323');
+            cy.get('button[type=submit]').as('sbmitBtn').click();
 
-        cy.get('button[type=submit]')
-            .as('sbmitBtn').click();
         cy.wait(3000);
     });
-
 });
+    it('Login without password', () => {
+        cy.fixture('LoginData').then(function (LoginData) {
+            this.LoginData = LoginData;
+            cy.get('input[name="email"]').type(this.LoginData.email)
+        
+            cy.get('button[type=submit]').as('sbmitBtn').click();
+            cy.contains('Hasło jest wymagane').should('be.visible')
+
+        cy.wait(3000);
+    });
+});
+
+    it('Login without email', () => {
+        cy.fixture('LoginData').then(function (LoginData) {
+            this.LoginData = LoginData;
+            cy.get('input[name="password"]').type(this.LoginData.password)
+        
+            cy.get('button[type=submit]').as('sbmitBtn').click();
+            cy.contains('E-mail jest wymagany').should('be.visible')
+
+        cy.wait(3000);
+    });
+});
+
+    it('Login with a short password', () => {
+        cy.fixture('LoginData').then(function (LoginData) {
+            this.LoginData = LoginData;
+            cy.get('input[name="email"]').type(this.LoginData.email)
+            cy.get('input[name="password"]').type('12345')
+    
+            cy.get('button[type=submit]').as('sbmitBtn').click();
+            cy.contains('Hasło musi zawierać przynajmniej 8 znaków').should('be.visible')
+
+        cy.wait(3000);
+    });
+});
+
     it('Password Reset', () => {
-        cy.viewport(1920,1080);
-        cy.visit('/login');
-        cy.wait(2000);
-        cy.contains('h1', 'Zaloguj się!')
-            .should('be.visible');
-
-        cy.get('span[role="button"]')
-        .click();
+        cy.get('span[role="button"]').click();
         cy.get('input[name="passwordreset"]').type('test@gmail.com');
+        cy.contains('button', 'Resetuj').click();
+    });
 });
-});
+
+
 
 
