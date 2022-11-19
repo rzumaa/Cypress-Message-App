fe1061a38df43b64c10c9c4fbe7754cd355d13da
Paula <paulinakocha@gmail.com>
2022-11-19 18:47:34 +0100

    Change AccountSettings test

--- cypress/e2e/AccountSettings.cy.js
+++ cypress/e2e/AccountSettings.cy.js
@@ -1,29 +1,40 @@
-describe('Chat', () => {
-            it('Chat', () => {
-                cy.visit('/login');
+describe('Account settings', function ()  {
+    before('Chat', () => {
+        cy.visit('/login');
+        cy.contains('h1', 'Zaloguj się!').should('be.visible');
+        cy.contains('Zalogowano pomyślnie!').should('be.visible')
 
-                cy.contains('h1', 'Zaloguj się!')
-                    .should('be.visible');
+        cy.fixture('LoginData').then(function (LoginData) {
+            this.LoginData = LoginData;
+            cy.get('input[name="email"]').type(this.LoginData.email)//.should('be.value', 'test@gmail.com');
+            cy.get('input[name="password"]').type(this.LoginData.password).should('be.value', '12345678');
+            cy.get('button[type=submit]').as('sbmitBtn').click();
+            cy.contains('span', 'Ustawienia').click();
+        });
+    });
 
-                cy.fixture('LoginData').then(function (LoginData) {
-                    this.LoginData = LoginData;
-                    cy.get('input[name="email"]').type(this.LoginData.email).should('be.value', 'test@gmail.com');
-                    cy.get('input[name="password"]').type(this.LoginData.password).should('be.value', '12345678');
-                    cy.get('button[type=submit]').as('sbmitBtn').click();
-                });
+    it('Add image', function () {
 
-        cy.contains('span', 'Ustawienia').click();
         cy.get('button[title="Edytuj zdjęcie"]').click();
         cy.get('input[type=file').attachFile("/golden.png");
         cy.contains('span', 'Wyślij').click();
+        cy.contains('Zdjęcie przesłano pomyślnie').should('be.visible')
+
+    });
+
+    it('Rename name', function () {
 
         cy.get('button[title="Edytuj nazwę"]').click();
         cy.contains('h3', 'Zmiana nazwy użytkownika').should('be.visible');
         cy.get('input[placeholder="Nazwa użytkownika"]').type('Pauliśka Testerka');
         cy.contains('button', 'Aktualizuj konto').click();
-        cy.wait(4000);
+        cy.contains('Nazwa użytkownika zmieniona').should('be.visible')
+             
         cy.contains('h3', 'Pauliśka Testerka').should('be.visible');
 
+    });
+
+    it('Change password', function () { 
 
         cy.get('button[title="Edytuj hasło"]').click();
         cy.get('#transition-modal-title').should('be.visible', 'Zmiana hasła');
@@ -36,7 +47,8 @@ describe('Chat', () => {
             cy.get('input[placeholder="Potwierdź nowe hasło"]').type(this.LoginData.Newpassword);
         });
         cy.contains('button', 'Zmień hasło').click();
-
+        cy.contains('Zaktualizowano pomyślnie').should('be.visible')
 
     });
 });
+
