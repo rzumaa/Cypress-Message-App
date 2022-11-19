c57d07f2c5f08893b735b49581979d6b1157359e
Paula <paulinakocha@gmail.com>
2022-11-19 17:58:06 +0100

    Change registration test

--- cypress/e2e/Registration.cy.js
+++ cypress/e2e/Registration.cy.js
@@ -1,4 +1,4 @@
-describe('rejestracja', () => {
+describe('User registration into application in form', () => {
     beforeEach(() => {
         cy.viewport(1920,1080);
         cy.visit('/login');
@@ -9,7 +9,7 @@ describe('rejestracja', () => {
         cy.get('button[type=submit]').as('smbitBtn');
     })
   
-    it('User registration into application in form', function () {
+    it('Successfully registration', function () {
 
         cy.fixture('RegistrationData').then(function (RegistrationData) {
             this.RegistrationData = RegistrationData;
@@ -67,4 +67,104 @@ describe('rejestracja', () => {
         cy.contains('Hasło jest wymagane').should('be.visible');
         cy.contains('Potwierdź hasło').should('be.visible');
      });
+     it('Should error for empty firstname', function () {
+
+        cy.fixture('RegistrationData').then(function (RegistrationData) {
+            this.RegistrationData = RegistrationData;
+            cy.get('input[name="email"]').type(this.RegistrationData.email);
+            cy.get('input[name="lastName"]').type(this.RegistrationData.lastname);
+            cy.get('input[name="password"]').type(this.RegistrationData.password);
+            cy.get('input[name="passwordConfirm"]').type(this.RegistrationData.passwordConfirm);
+        });
+
+        cy.get('@smbitBtn').click();
+        cy.contains('Imię jest wymagane').should('be.visible');
+     });
+
+     it('Should error for empty lastname', function () {
+
+        cy.fixture('RegistrationData').then(function (RegistrationData) {
+            this.RegistrationData = RegistrationData;
+            cy.get('input[name="email"]').type(this.RegistrationData.email);
+            cy.get('input[name="firstName"]').type(this.RegistrationData.firstname);
+            cy.get('input[name="password"]').type(this.RegistrationData.password);
+            cy.get('input[name="passwordConfirm"]').type(this.RegistrationData.passwordConfirm);
+        });
+
+        cy.get('@smbitBtn').click();
+        cy.contains('Nazwisko jest wymagane').should('be.visible');
+     });
+
+     it('Should error for empty email', function () {
+
+        cy.fixture('RegistrationData').then(function (RegistrationData) {
+            this.RegistrationData = RegistrationData;
+            cy.get('input[name="firstName"]').type(this.RegistrationData.firstname);
+            cy.get('input[name="lastName"]').type(this.RegistrationData.lastname);
+            cy.get('input[name="password"]').type(this.RegistrationData.password);
+            cy.get('input[name="passwordConfirm"]').type(this.RegistrationData.passwordConfirm);
+        });
+
+        cy.get('@smbitBtn').click();
+        cy.contains('E-mail jest wymagany').should('be.visible');
+     });
+
+     it('Should error for  email without "@"', function () {
+
+        cy.fixture('RegistrationData').then(function (RegistrationData) {
+            this.RegistrationData = RegistrationData;
+            cy.get('input[name="email"]').type(this.RegistrationData.incorectemail);
+            cy.get('input[name="firstName"]').type(this.RegistrationData.firstname);
+            cy.get('input[name="lastName"]').type(this.RegistrationData.lastname);
+            cy.get('input[name="password"]').type(this.RegistrationData.password);
+            cy.get('input[name="passwordConfirm"]').type(this.RegistrationData.passwordConfirm);
+        });
+
+        cy.get('@smbitBtn').click();
+        cy.contains('Podany adres jest niepoprawny').should('be.visible');
+     });
+     it('Should error for  email without "@gmail.com"', function () {
+
+        cy.fixture('RegistrationData').then(function (RegistrationData) {
+            this.RegistrationData = RegistrationData;
+            cy.get('input[name="email"]').type(this.RegistrationData.incorectemail2);
+            cy.get('input[name="firstName"]').type(this.RegistrationData.firstname);
+            cy.get('input[name="lastName"]').type(this.RegistrationData.lastname);
+            cy.get('input[name="password"]').type(this.RegistrationData.password);
+            cy.get('input[name="passwordConfirm"]').type(this.RegistrationData.passwordConfirm);
+        });
+
+        cy.get('@smbitBtn').click();
+        cy.contains('Podany adres jest niepoprawny').should('be.visible');
+     });
+     it('Should error for  email without "gmail"', function () {
+
+        cy.fixture('RegistrationData').then(function (RegistrationData) {
+            this.RegistrationData = RegistrationData;
+            cy.get('input[name="email"]').type(this.RegistrationData.incorectemail3);
+            cy.get('input[name="firstName"]').type(this.RegistrationData.firstname);
+            cy.get('input[name="lastName"]').type(this.RegistrationData.lastname);
+            cy.get('input[name="password"]').type(this.RegistrationData.password);
+            cy.get('input[name="passwordConfirm"]').type(this.RegistrationData.passwordConfirm);
+        });
+
+        cy.get('@smbitBtn').click();
+        cy.contains('Podany adres jest niepoprawny').should('be.visible');
+     });
+
+     it('Should error for  email without ".com"', function () {
+
+        cy.fixture('RegistrationData').then(function (RegistrationData) {
+            this.RegistrationData = RegistrationData;
+            cy.get('input[name="email"]').type(this.RegistrationData.incorectemail4);
+            cy.get('input[name="firstName"]').type(this.RegistrationData.firstname);
+            cy.get('input[name="lastName"]').type(this.RegistrationData.lastname);
+            cy.get('input[name="password"]').type(this.RegistrationData.password);
+            cy.get('input[name="passwordConfirm"]').type(this.RegistrationData.passwordConfirm);
+        });
+
+        cy.get('@smbitBtn').click();
+        cy.contains('Podany adres jest niepoprawny').should('be.visible');
+     });
+
 });
