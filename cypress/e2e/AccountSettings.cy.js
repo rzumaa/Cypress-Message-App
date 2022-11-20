3817b572e0843411f8f575869cc9efb78974e2f1
Paula <paulinakocha@gmail.com>
2022-11-20 13:00:24 +0100

    Add comments

--- cypress/e2e/AccountSettings.cy.js
+++ cypress/e2e/AccountSettings.cy.js
@@ -2,15 +2,21 @@ describe('Account settings', function ()  {
     before('Chat', () => {
         cy.visit('/login');
         cy.contains('h1', 'Zaloguj się!').should('be.visible');
-        cy.contains('Zalogowano pomyślnie!').should('be.visible')
 
         cy.fixture('LoginData').then(function (LoginData) {
             this.LoginData = LoginData;
             cy.get('input[name="email"]').type(this.LoginData.email)//.should('be.value', 'test@gmail.com');
             cy.get('input[name="password"]').type(this.LoginData.password).should('be.value', '12345678');
             cy.get('button[type=submit]').as('sbmitBtn').click();
-            cy.contains('span', 'Ustawienia').click();
         });
+
+        // we should have a visible notice now
+        cy.contains('Zalogowano pomyślnie!').should('be.visible');
+        
+        // Should be on a new URL
+        cy.url().should('include', 'http://localhost:3000/');
+
+        cy.contains('span', 'Ustawienia').click();
     });
 
     it('Add image', function () {
@@ -18,6 +24,8 @@ describe('Account settings', function ()  {
         cy.get('button[title="Edytuj zdjęcie"]').click();
         cy.get('input[type=file').attachFile("/golden.png");
         cy.contains('span', 'Wyślij').click();
+
+        // we should have a visible notice now
         cy.contains('Zdjęcie przesłano pomyślnie').should('be.visible')
 
     });
@@ -28,6 +36,8 @@ describe('Account settings', function ()  {
         cy.contains('h3', 'Zmiana nazwy użytkownika').should('be.visible');
         cy.get('input[placeholder="Nazwa użytkownika"]').type('Pauliśka Testerka');
         cy.contains('button', 'Aktualizuj konto').click();
+
+        // we should have a visible notice now
         cy.contains('Nazwa użytkownika zmieniona').should('be.visible')
              
         cy.contains('h3', 'Pauliśka Testerka').should('be.visible');
@@ -47,6 +57,8 @@ describe('Account settings', function ()  {
             cy.get('input[placeholder="Potwierdź nowe hasło"]').type(this.LoginData.Newpassword);
         });
         cy.contains('button', 'Zmień hasło').click();
+
+        // we should have a visible notice now
         cy.contains('Zaktualizowano pomyślnie').should('be.visible')
 
     });
